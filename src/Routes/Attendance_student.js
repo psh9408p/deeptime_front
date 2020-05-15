325a0766ea00a5d880aeb7fd5101148a64edd3ab
Sanghun Jeong <ertr777@naver.com>
2020-05-15 13:58:25 +0900

    업데이트

--- src/Routes/Attendance_student.js
+++ src/Routes/Attendance_student.js
@@ -1,37 +1,37 @@
-import React, { useState } from "react"
-import styled from "styled-components"
-import Button_blue from "../Components/Buttons/Button_inputVer"
-import Button_red from "../Components/Buttons/Button_red"
-import useInput from "../Hooks/useInput"
-import Textarea from "../Components/Textarea"
-import { gql } from "apollo-boost"
-import { useMutation } from "react-apollo-hooks"
-import { toast } from "react-toastify"
-import NumericInput from "react-numeric-input"
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Button_blue from '../Components/Buttons/Button_inputVer';
+import Button_red from '../Components/Buttons/Button_red';
+import useInput from '../Hooks/useInput';
+import Textarea from '../Components/Textarea';
+import { gql } from 'apollo-boost';
+import { useMutation } from 'react-apollo-hooks';
+import { toast } from 'react-toastify';
+import NumericInput from 'react-numeric-input';
 
 const CHECK_ATTENDANCE = gql`
   mutation checkAttendance($email: String!) {
     checkAttendance(email: $email)
   }
-`
+`;
 
 const END_ATTENDANCE = gql`
   mutation endAttendance($email: String!) {
     endAttendance(email: $email)
   }
-`
+`;
 
 const RE_ATTENDANCE = gql`
   mutation reAttendance($email: String!) {
     reAttendance(email: $email)
   }
-`
+`;
 
 const CHECK_ABSENCE = gql`
   mutation checkAbsence($email: String!, $absenceReason: String!) {
     checkAbsence(email: $email, absenceReason: $absenceReason)
   }
-`
+`;
 
 const EDIT_TARGETTIMEBOX = gql`
   mutation edit_myTargetTimeBox(
@@ -53,14 +53,14 @@ const EDIT_TARGETTIMEBOX = gql`
       sunday: $sunday
     )
   }
-`
+`;
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const ButtonWrap = styled.div`
   width: 130px;
@@ -68,7 +68,7 @@ const ButtonWrap = styled.div`
   &:not(:first-child) {
     margin-left: 20px;
   }
-`
+`;
 
 const AbsenceBox = styled.form`
   width: 590px;
@@ -80,7 +80,7 @@ const AbsenceBox = styled.form`
   flex-direction: column;
   align-items: center;
   margin-top: 15px;
-`
+`;
 
 const AttendanceBox = styled.div`
   width: 590px;
@@ -92,7 +92,7 @@ const AttendanceBox = styled.div`
   align-items: center;
   justify-content: center;
   margin-top: 50px;
-`
+`;
 
 const MyStatus = styled.div`
   width: 240px;
@@ -102,7 +102,7 @@ const MyStatus = styled.div`
   span {
     color: black;
   }
-`
+`;
 
 const EtcButtonWrap = styled.div`
   width: 100%;
@@ -111,7 +111,7 @@ const EtcButtonWrap = styled.div`
   justify-content: flex-end;
   padding-right: 24px;
   margin-top: 24px;
-`
+`;
 
 const ReasonText = styled(Textarea)`
   width: 542px;
@@ -119,7 +119,7 @@ const ReasonText = styled(Textarea)`
   margin-top: 15px;
   display: inline-block;
   margin-bottom: 15px;
-`
+`;
 
 const TargetBoxRow = styled.div`
   width: 100%;
@@ -136,13 +136,13 @@ const TargetBoxRow = styled.div`
     height: 35%;
     justify-content: flex-end;
   }
-`
+`;
 
 const TargetBoxButtonWrap = styled.div`
   width: 130px;
   height: 33px;
   margin-right: 20px;
-`
+`;
 
 const InputAreaColumn = styled.div`
   display: flex;
@@ -152,7 +152,7 @@ const InputAreaColumn = styled.div`
   &:not(:first-child) {
     margin-left: 15px;
   }
-`
+`;
 const InputText = styled.div`
   font-size: 16px;
   font-weight: 600;
@@ -163,26 +163,40 @@ const InputText = styled.div`
   &:last-child {
     margin-top: 10px;
   }
-`
+`;
 
 const TargetBoxTitle = styled.div`
   margin-top: 10px;
   font-size: 16px;
   font-weight: 600;
   color: ${(props) => props.theme.classicBlue};
-`
+`;
 
-const Day_7 = ["월", "화", "수", "목", "금", "토", "일"]
+const Day_7 = ['월', '화', '수', '목', '금', '토', '일'];
 
 export default ({ Mydata, MyRefetch }) => {
-  const StatusReason = useInput(Mydata.me.todayTime.absenceReason)
-  const [targetTime_1, setTargetTime_1] = useState(Mydata.me.targetTimeBox.monday) // 월
-  const [targetTime_2, setTargetTime_2] = useState(Mydata.me.targetTimeBox.tuesday)
-  const [targetTime_3, setTargetTime_3] = useState(Mydata.me.targetTimeBox.wednesday)
-  const [targetTime_4, setTargetTime_4] = useState(Mydata.me.targetTimeBox.thursday)
-  const [targetTime_5, setTargetTime_5] = useState(Mydata.me.targetTimeBox.friday)
-  const [targetTime_6, setTargetTime_6] = useState(Mydata.me.targetTimeBox.saturday)
-  const [targetTime_7, setTargetTime_7] = useState(Mydata.me.targetTimeBox.sunday) // 일
+  const StatusReason = useInput(Mydata.me.todayTime.absenceReason);
+  const [targetTime_1, setTargetTime_1] = useState(
+    Mydata.me.targetTimeBox.monday,
+  ); // 월
+  const [targetTime_2, setTargetTime_2] = useState(
+    Mydata.me.targetTimeBox.tuesday,
+  );
+  const [targetTime_3, setTargetTime_3] = useState(
+    Mydata.me.targetTimeBox.wednesday,
+  );
+  const [targetTime_4, setTargetTime_4] = useState(
+    Mydata.me.targetTimeBox.thursday,
+  );
+  const [targetTime_5, setTargetTime_5] = useState(
+    Mydata.me.targetTimeBox.friday,
+  );
+  const [targetTime_6, setTargetTime_6] = useState(
+    Mydata.me.targetTimeBox.saturday,
+  );
+  const [targetTime_7, setTargetTime_7] = useState(
+    Mydata.me.targetTimeBox.sunday,
+  ); // 일
 
   const editTargetTimeBoxMutation = useMutation(EDIT_TARGETTIMEBOX, {
     variables: {
@@ -194,133 +208,133 @@ export default ({ Mydata, MyRefetch }) => {
       saturday: targetTime_6,
       sunday: targetTime_7,
     },
-  })
+  });
 
   const checkAttendanceMutation = useMutation(CHECK_ATTENDANCE, {
     variables: {
       email: Mydata.me.email,
     },
-  })
+  });
 
   const endAttendanceMutation = useMutation(END_ATTENDANCE, {
     variables: {
       email: Mydata.me.email,
     },
-  })
+  });
 
   const reAttendanceMutation = useMutation(RE_ATTENDANCE, {
     variables: {
       email: Mydata.me.email,
     },
-  })
+  });
 
   const checkAbsenceMutation = useMutation(CHECK_ABSENCE, {
     variables: {
       email: Mydata.me.email,
       absenceReason: StatusReason.value,
     },
-  })
+  });
 
   const func_attendance = async () => {
     try {
-      toast.info("출석 처리 중...")
+      toast.info('출석 처리 중...');
       const {
         data: { checkAttendance },
-      } = await checkAttendanceMutation()
+      } = await checkAttendanceMutation();
       if (!checkAttendance) {
-        toast.error("출석이 불가능합니다.")
+        toast.error('출석이 불가능합니다.');
       } else {
-        await MyRefetch()
-        toast.success("출석이 완료되었습니다.")
+        await MyRefetch();
+        toast.success('출석이 완료되었습니다.');
       }
     } catch (e) {
-      const realText = e.message.split("GraphQL error: ")
-      toast.error(realText[1])
+      const realText = e.message.split('GraphQL error: ');
+      toast.error(realText[1]);
     }
-  }
+  };
 
   const func_endAttendance = async () => {
     try {
-      toast.info("학습 종료 중...")
+      toast.info('학습 종료 중...');
       const {
         data: { endAttendance },
-      } = await endAttendanceMutation()
+      } = await endAttendanceMutation();
       if (!endAttendance) {
-        toast.error("학습 종료가 불가능합니다.")
+        toast.error('학습 종료가 불가능합니다.');
       } else {
-        await MyRefetch()
-        toast.success("학습이 종료되었습니다.")
+        await MyRefetch();
+        toast.success('학습이 종료되었습니다.');
       }
     } catch (e) {
-      const realText = e.message.split("GraphQL error: ")
-      toast.error(realText[1])
+      const realText = e.message.split('GraphQL error: ');
+      toast.error(realText[1]);
     }
-  }
+  };
 
   const func_reAttendance = async () => {
     try {
-      toast.info("재학습을 위해 출석 상태 변경 중...")
+      toast.info('재학습을 위해 출석 상태 변경 중...');
       const {
         data: { reAttendance },
-      } = await reAttendanceMutation()
+      } = await reAttendanceMutation();
       if (!reAttendance) {
-        toast.error("출석 상태 변경이 불가능합니다.")
+        toast.error('출석 상태 변경이 불가능합니다.');
       } else {
-        await MyRefetch()
+        await MyRefetch();
         toast.success(
           <div>
             출석 상태 변경이 완료되었습니다.
             <br />
             다시 학습이 가능합니다.
-          </div>
-        )
+          </div>,
+        );
       }
     } catch (e) {
-      const realText = e.message.split("GraphQL error: ")
-      toast.error(realText[1])
+      const realText = e.message.split('GraphQL error: ');
+      toast.error(realText[1]);
     }
-  }
+  };
 
   const func_absence = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      toast.info("조퇴 처리 중...")
+      toast.info('조퇴 처리 중...');
       const {
         data: { checkAbsence },
-      } = await checkAbsenceMutation()
+      } = await checkAbsenceMutation();
       if (!checkAbsence) {
-        toast.error("조퇴 처리가 불가능합니다.")
+        toast.error('조퇴 처리가 불가능합니다.');
       } else {
-        await MyRefetch()
-        toast.success("조퇴 처리가 완료되었습니다.")
+        await MyRefetch();
+        toast.success('조퇴 처리가 완료되었습니다.');
       }
     } catch (e) {
-      const realText = e.message.split("GraphQL error: ")
-      toast.error(realText[1])
+      const realText = e.message.split('GraphQL error: ');
+      toast.error(realText[1]);
     }
-  }
+  };
 
   const edit_targetTimeBox = async () => {
     try {
-      toast.info("목표시간 수정 중...")
+      toast.info('목표시간 수정 중...');
       const {
         data: { edit_myTargetTimeBox },
-      } = await editTargetTimeBoxMutation()
+      } = await editTargetTimeBoxMutation();
       if (!edit_myTargetTimeBox) {
-        toast.error("목표시간 수정이 불가능합니다.")
+        toast.error('목표시간 수정이 불가능합니다.');
       } else {
-        await MyRefetch()
-        toast.success("목표시간 수정이 완료되었습니다.")
+        await MyRefetch();
+        toast.success('목표시간 수정이 완료되었습니다.');
       }
     } catch (e) {
-      const realText = e.message.split("GraphQL error: ")
-      toast.error(realText[1])
+      const realText = e.message.split('GraphQL error: ');
+      toast.error(realText[1]);
     }
-  }
+  };
 
   const myFormat = (num) => {
-    return num + "시간"
-  }
+    return num + '시간';
+  };
 
   return (
     <Wrapper>
@@ -329,22 +343,30 @@ export default ({ Mydata, MyRefetch }) => {
           오늘의 출석 상태: <span>{Mydata.me.todayTime.attendanceStatus}</span>
         </MyStatus>
         <ButtonWrap>
-          <Button_blue type="button" text={"출석하기~!"} onClick={func_attendance} />
+          <Button_blue
+            type="button"
+            text={'출석하기~!'}
+            onClick={func_attendance}
+          />
         </ButtonWrap>
         <ButtonWrap>
-          <Button_red type="button" text={"오늘 학습 끝~!"} onClick={func_endAttendance} />
+          <Button_red
+            type="button"
+            text={'오늘 학습 끝~!'}
+            onClick={func_endAttendance}
+          />
         </ButtonWrap>
       </AttendanceBox>
       <AbsenceBox onSubmit={func_absence}>
         <EtcButtonWrap>
           <ButtonWrap>
-            <Button_blue text={"다시 공부하기~!"} onClick={func_reAttendance} />
+            <Button_blue text={'다시 공부하기~!'} onClick={func_reAttendance} />
           </ButtonWrap>
           <ButtonWrap>
-            <Button_red type="submit" text={"조퇴하기"} />
+            <Button_red type="submit" text={'조퇴하기'} />
           </ButtonWrap>
         </EtcButtonWrap>
-        <ReasonText placeholder={"조퇴 사유"} {...StatusReason} />
+        <ReasonText placeholder={'조퇴 사유'} {...StatusReason} />
       </AbsenceBox>
       <AbsenceBox>
         <TargetBoxRow>
@@ -358,21 +380,27 @@ export default ({ Mydata, MyRefetch }) => {
                 <NumericInput
                   min={1}
                   max={24}
-                  value={eval("targetTime_" + (index + 1)) / 3600}
+                  value={eval('targetTime_' + (index + 1)) / 3600}
                   size={1}
                   format={myFormat}
-                  onChange={(num) => eval("setTargetTime_" + (index + 1) + "(num * 3600)")}
+                  onChange={(num) =>
+                    eval('setTargetTime_' + (index + 1) + '(num * 3600)')
+                  }
                 />
               </InputAreaColumn>
-            )
+            );
           })}
         </TargetBoxRow>
         <TargetBoxRow>
           <TargetBoxButtonWrap>
-            <Button_blue type="button" text={"목표시간 변경"} onClick={edit_targetTimeBox} />
+            <Button_blue
+              type="button"
+              text={'목표시간 변경'}
+              onClick={edit_targetTimeBox}
+            />
           </TargetBoxButtonWrap>
         </TargetBoxRow>
       </AbsenceBox>
     </Wrapper>
-  )
-}
+  );
+};
--- src/Routes/Study.js
+++ src/Routes/Study.js
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default () => '스터디 준비중';
+export default () => '준비중';
